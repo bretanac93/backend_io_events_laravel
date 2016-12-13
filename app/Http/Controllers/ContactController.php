4115854109af0750150f04d7c5d41fd4062a448f
Cesar Bretana Gonzalez <bretanac@gmail.com>
2016-12-13 16:11:33 -0500

    Some old work

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 use Auth;
 
 class ContactController extends Controller
@@ -14,20 +15,24 @@ class ContactController extends Controller
 	// GET /contacts
 	public function index(Request $request) {
 		$user = Auth::user();
-		// $followers = request()->user()->followers();
-		// $following = request()->user()->following;
+		$followers = request()->user()->followers;
+		$following = request()->user()->following;
 
-		// $contacts = $followers->merge($following);
+		$contacts = $followers->merge($following);
 
-		return response()->json($user);
+		return $contacts;
 	}
 	// Add a new contact
 	// Logic: Follow a person based on they email (phone number to future)
 
 	// POST /contacts
 	public function store(Request $request) {
-		dd($request->user());
-		return response()->json(["data" => "OK"]);
+		$to_follow = User::find($request->get('to_follow'));
+
+		$user = Auth::user();
+		$user->follow($to_follow);
+		
+		return response()->json(['data' => "$user followed $to_follow"]);
 	}
 	// Show a contact
 	// Logic: Retrieve a contact details based on his email
